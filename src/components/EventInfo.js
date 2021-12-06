3c00734aff621c005bb00277b5d87172949f7ede
CristianP50 <cristianjp.padierna2@gmail.com>
2021-12-05 20:47:26 -0500

    MainPage actualizado

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Event from "./Event";
+
+
 const EventInfo = () => {
+
+  const { data } = useSelector((state) => state.database);
+  const [prodSelec, setProdSelec] = useState(null);
+  const params = useParams();
+
+
+  useEffect(() => {
+    if (data !== null) {
+      setProdSelec(data.filter((el) => el.eventName === params.eventsName)[0]);
+    }
+  }, [data, params.eventsName]);
+
+
   return (
-    <div>
+    <>
       <h2>Soy un EventInfo</h2>
-    </div>
+      <Link to="/">
+        <button>Atras</button>
+      </Link>
+      {prodSelec === null ? (
+        <h3>Cargando...</h3>
+      ) : (
+        <h1>hi</h1>
+      )}
+    </>
   );
 };
 
