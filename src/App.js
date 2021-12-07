e654e2c37fd31099ff95fa247f69964ad41b11ed
daniel <daniel.zapata.682@gmail.com>
2021-12-06 23:42:15 -0500

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MainPage from "./components/MainPage";
+import NavMenu from "./components/NavMenu";
+import UserInfo from "./components/UserInfo";
 
 export const App = () => {
-
   return (
     <div>
+      <NavMenu />
       <MainPage />
     </div>
   );
--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -2,6 +2,19 @@ const AddEvent = () => {
   return (
     <div>
       <h2>Soy un AddEvent</h2>
+      <p>Formulario para añadir un nuevo evento</p>
+      <p>event-name</p>
+      <p>category</p>
+      <p>date</p>
+      <p>organization</p>
+      <p>address</p>
+      <p>telephone</p>
+      <p>cellphone</p>
+      <p>email</p>
+      <p>description</p>
+      <p>img-url</p>
+      <p>price</p>
+      <p>website</p>
     </div>
   );
 };
